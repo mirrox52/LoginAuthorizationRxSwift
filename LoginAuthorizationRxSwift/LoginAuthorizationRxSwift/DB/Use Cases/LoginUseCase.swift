9e53133601e7279da7f3afe12ecefeb27c5fcaed
Aliaksandr Batyesheu <mirrox@tut.by>
2021-01-19 21:28:15 +0300

    Begin to refactor project with Itexus MVVM

---
+++ LoginAuthorizationRxSwift/LoginAuthorizationRxSwift/DB/Use Cases/LoginUseCase.swift
@@ -0,0 +1,15 @@
+//
+//  LoginUseCase.swift
+//  LoginAuthorizationRxSwift
+//
+//  Created by Aliaksandr on 19.01.21.
+//
+
+import Foundation
+import RxSwift
+import RxCocoa
+import RealmSwift
+
+class LoginUseCase {
+    
+}
---
+++ LoginAuthorizationRxSwift/LoginAuthorizationRxSwift/DB/Use Cases/SignUpUseCase.swift
@@ -0,0 +1,15 @@
+//
+//  SignUpUseCase.swift
+//  LoginAuthorizationRxSwift
+//
+//  Created by Aliaksandr on 19.01.21.
+//
+
+import Foundation
+import RxCocoa
+import RxSwift
+import RealmSwift
+
+class SignUpUseCase {
+    
+}
